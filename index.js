a3142fb8859b6dd10fe0b9d26773a0b4b307ac7a
Git Gud <mitchw2002@gmail.com>
2017-02-03 09:46:40 +1100

    Fixed socket.io thing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+//const io = require('socket.io')(http);
 
 const chalk = require('chalk');
 const path = require('path');
